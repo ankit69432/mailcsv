dbcae30fc60d29813d2b06bc3d529a8dff7c4511
Ankit Mishra <ankit.itwankalan@gmail.com>
2023-06-12 15:46:09 +0530

    first commit

--- api/db/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const con = new Sequelize("csv", "ankit", "", {
-  dialect: "postgres",
-  host: "localhost",
-});
-
-module.exports = con;
--- api/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { createUser, mail } = require("../controllers/controller");
-const router = express.Router();
-
-router.post("/user", createUser).post("/mail", mail);
-
-module.exports = router;
